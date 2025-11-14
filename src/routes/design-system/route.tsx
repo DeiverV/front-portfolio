8080d1ef9fbe5cea9836bed800edb7c1e16114fe
DeiberV <deiberveranodev@gmail.com>
2025-11-14 15:42:00 -0500

    Feat: i18n on working on this text

--- src/routes/design-system/route.tsx
+++ src/routes/design-system/route.tsx
@@ -1,17 +1,16 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next'
 
 export const Route = createFileRoute('/design-system')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { t } = useTranslation('common')
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-neutral">
       <p className="text-text text-9xl">😴</p>
-      <h1 className="text-text text-4xl">
-        <br />
-        Hola! estoy trabajando en esto en este momento
-      </h1>
+      <h1 className="text-text text-4xl">{t('workingOnThis')}</h1>
       <Link
         to="/"
         viewTransition
